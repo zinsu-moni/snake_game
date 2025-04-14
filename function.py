6bff09c5be758325ce11c3af85398727fa4a7901
zinsu-moni <143900950+zinsu-moni@users.noreply.github.com>
2025-04-14 10:41:36 +0100

    Update function.py

--- function.py
+++ function.py
@@ -20,4 +20,5 @@ class Scoreboard(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
+        self.color("red")
         self.write("GAME OVER", align="center", font=("Courier", 30, "bold"))
